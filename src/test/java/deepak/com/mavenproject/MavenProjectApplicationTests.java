b623724fcc686f33b4f3cea6fad2613b01cae1c3
imdeepaktheja <130925058+imdeepaktheja@users.noreply.github.com>
2023-12-29 23:55:47 +0530

    initial commit

---
+++ src/test/java/deepak/com/mavenproject/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package deepak.com.mavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
